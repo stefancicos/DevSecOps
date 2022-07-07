c96036468edc0cf1c83b64e90a3129fc8dc58ccd
stefancicos <77525072+stefancicos@users.noreply.github.com>
2022-07-07 13:10:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,12 @@ app = Flask(__name__)
 def run_cmd(cmd):
 	return subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read()
 
-@app.route('/<cmd>')
+@app.route('/cmdinj/<cmd>')
 def main(cmd):
 	number = 9
 	return str(run_cmd(cmd))+"<hr>"+str(exec("number*number"))+"<hr>"+str(eval("number + number"))
+
+@app.route('/pyinj/<cmd>')
+def pyinf(cmd):
 
 serve(app, port=8081, host='0.0.0.0')
