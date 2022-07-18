5f47aeb80a00d6e720a95113b00bdcd37f8283a8
stefancicos <77525072+stefancicos@users.noreply.github.com>
2022-07-18 15:14:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask
-import subprocess
 import os
-import ftplib
 from flask import request
 from waitress import serve
 
 app = Flask(__name__)
 
 def run_cmd(cmd):
-	return subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read()
+	return str(cmd)
 
 @app.route('/cmdinj/<cmd>')
 def main(cmd):
@@ -16,7 +14,7 @@ def main(cmd):
 
 @app.route('/pyevalinj/<cmd>')
 def pyevalinj(cmd):
-	return str(eval(cmd))
+	return str(cmd)
 
 @app.route('/<text>')
 def xss(text = None):
@@ -25,8 +23,4 @@ def xss(text = None):
 	else:
 		return "Bet ya can't break me!"
 
-@app.route('/sql/<cmd>')
-def sqli(cmd):
-	query = "DELETE FROM foo WHERE id = '%s'" % cmd
-	return "No sqli here :)"
 serve(app, port=8081, host='192.168.201.204')
